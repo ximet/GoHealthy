0910e4f6f05e9a07e1444055b565baeca6a39428
ximet <dq.ximet@gmail.com>
2017-01-11 20:05:13 +0300

    update localStorage service

--- src/services/LocalStorageService.js
+++ src/services/LocalStorageService.js
@@ -1,25 +1,41 @@
 import { AsyncStorage } from 'react-native';
 
-export const storageSet = (key, data) => {
-    AsyncStorage.setItem(key, data)
-        .then(result => console.log(result))
-        .catch(error => console.log(error))
-};
+export const LocaleStorage = {
+    set (key, data) {
+        AsyncStorage.setItem(key, JSON.stringify(data))
+            .then(() => console.log(`Set data with key ${key} to LocalStorage`));
+    },
 
-export const storageGet = (key) => {
-    return AsyncStorage.getItem(key)
-        .then(result => value !== null ? value : null)
-        .catch(error => console.log(error))
-};
+    setArray (keyArray, dataArray) {
+        for(let i = 0; i < keyArray.length; i++){
+            AsyncStorage.setItem(keyArray[i], JSON.stringify(dataArray[i]))
+                .then(() => console.log(`Set data with key ${keyArray[i]} to LocalStorage`));
+        }
+    },
 
-export const removeItemFromStorage = (key) => {
-    AsyncStorage.removeItem(key)
-        .then(result => console.log(result))
-        .catch(error => console.log(error))
-};
+    get (key) {
+        return AsyncStorage.getItem(key)
+            .then(data => {
+                return JSON.parse(data);
+            });
+    },
 
-export const clearStorage = (key) => {
-    AsyncStorage.clear()
-        .then(result => console.log(result))
-        .catch(error => console.log(error))
+    getArrayItem (keyArray) {
+        return AsyncStorage.multiGet(keyArray)
+                    .then((data) => {
+                        return data.map(item => {
+                            return JSON.parse(item[1]);
+                        });
+        });
+    },
+
+    remove (key) {
+        AsyncStorage.removeItem(key)
+            .then(() => console.log(`Delete data with key ${key} to LocalStorage`));
+    },
+
+    clear () {
+        AsyncStorage.clear()
+            .then(() => console.log(`LocalStorage is clear `))
+    }
 };
