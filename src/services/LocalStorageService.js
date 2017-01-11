289d5eea430ed9a154831d1c629f7ab5721cb6b2
ximet <dq.ximet@gmail.com>
2017-01-11 20:14:17 +0300

    add catch and log error for LocalStorageService

--- src/services/LocalStorageService.js
+++ src/services/LocalStorageService.js
@@ -18,19 +18,22 @@ const multiSet = (keys, values) => {
 export const LocaleStorage = {
     set (key, data) {
         AsyncStorage.setItem(key, JSON.stringify(data))
-            .then(() => console.log(`Set data with key ${key} to LocalStorage`));
+            .then(() => console.log(`Set data with key ${key} to LocalStorage`))
+            .catch((error) => console.log(`Something went wrong with set data. More info: ${error}`));
     },
 
     setArray (keyArray, dataArray) {
-        multiSet(keyArraym, dataArray)
-            .then(result => console.log(result));
+        multiSet(keyArray, dataArray)
+            .then(result => console.log(result))
+            .catch((error) => console.log(`Something went wrong with setArray data. More info: ${error}`));
     },
 
     get (key) {
         return AsyncStorage.getItem(key)
             .then(data => {
                 return JSON.parse(data);
-            });
+            })
+            .catch((error) => console.log(`Something went wrong with get data. More info: ${error}`));
     },
 
     getArrayItem (keyArray) {
@@ -38,17 +41,20 @@ export const LocaleStorage = {
                     .then((data) => {
                         return data.map(item => {
                             return JSON.parse(item[1]);
-                        });
+                        })
+                    .catch((error) => console.log(`Something went wrong with get Arraydata. More info: ${error}`));
         });
     },
 
     remove (key) {
         AsyncStorage.removeItem(key)
-            .then(() => console.log(`Delete data with key ${key} to LocalStorage`));
+            .then(() => console.log(`Delete data with key ${key} to LocalStorage`))
+            .catch((error) => console.log(`Something went wrong with delete data. More info: ${error}`));
     },
 
     clear () {
         AsyncStorage.clear()
             .then(() => console.log(`LocalStorage is clear `))
+            .catch((error) => console.log(`Something went wrong with clear localStorage. More info: ${error}`));
     }
 };
