f98ac8c67c0870527fe75c00c0227122b85bccdf
ximet <dq.ximet@gmail.com>
2017-01-15 13:15:36 +0300

    month view with mockObject

--- src/views/MonthView.js
+++ src/views/MonthView.js
@@ -8,6 +8,27 @@ import { getTitleCalendarFormat, getCurrentDate, monthNames, dayNames } from '..
 import Calendar from '../../components/Calendar/Calendar.js';
 import TextLabel from '../../components/TextLabel/TextLabel.js';
 import WeekList from '../../components/WeekList/WeekList.js';
+import Button from '../../components/Button/Button.js';
+
+
+const items = [
+    {   label: "Отоларинголог",
+        dateText: '8 Фев 2016',
+        timeText: '11:00 - 12:30',
+        centerText:'Нордин',
+        doctorText: 'ЛОР'
+    }
+];
+
+const getList = (items) => {
+    return items.length > 0
+        ? (<WeekList items={items}/>)
+        : (<TextLabel label={'На этот месяц ничего не запланировано.'} />)
+};
+
+const getFooter = (actionForButton) => {
+    return (<Button label={'Текущий месяц'} onPressButton={actionForButton} />)
+};
 
 export const MonthView = connect(monthViewSelector, monthViewActions)((props) => {
 
@@ -29,5 +50,8 @@ export const MonthView = connect(monthViewSelector, monthViewActions)((props) =>
                     onSwipePrev={() => console.log('Back SWIPE')}     // eslint-disable-line no-console
                     onSwipeNext={() => console.log('Forward SWIPE')}  // eslint-disable-line no-console
                 />
+
+                {getList(items)}
+                {getFooter(() => {})}
         </View>
 });
