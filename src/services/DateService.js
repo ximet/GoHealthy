d965cd74d5d699dd4ee36a679f828dea1c1166cf
ximet <dq.ximet@gmail.com>
2017-01-16 18:30:23 +0300

    update dateService

--- src/services/DateService.js
+++ src/services/DateService.js
@@ -12,6 +12,20 @@ export const getTitleCalendarFormat = () => {
     return 'MMMM YYYY';
 };
 
+export const getCurrentEndDate = () => {
+    return getCurrentDate().endOf('day');
+};
+
+export const getCurrentYear = () => {
+    return getCurrentDate().year();
+};
+
+export const getDifference = (startDate, endTimeValue) => {
+    const format = "DD/MM/YYYY HH:mm:ss";
+
+    return moment.utc(moment(endTimeValue, format).diff(moment(startDate, format)));
+};
+
 export const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
 export const dayNames = ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'];
