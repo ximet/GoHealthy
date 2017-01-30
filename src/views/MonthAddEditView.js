d9a3074e7afca794451c908e4086bfb6ee067470
ximet <dq.ximet@gmail.com>
2017-01-30 21:37:09 +0300

    add test datePicker

--- src/views/MonthAddEditView.js
+++ src/views/MonthAddEditView.js
@@ -4,6 +4,7 @@ import { View } from 'react-native';
 import { monthAddEditViewSelector } from '../selectors/monthAddEditViewSelector.js';
 import { monthAddEditViewActions, ADD_ITEM } from '../actions/monthAddEditViewActions.js';
 import { CHANGE_LABEL, CHANGE_PLACE, CHANGE_FULL_NAME, SAVE_ITEM } from '../actions/monthAddEditViewActions.js';
+import DatePicker from 'react-native-datepicker'
 
 
 import Calendar from '../../components/Calendar/Calendar.js';
@@ -12,7 +13,7 @@ import Input from '../../components/Input/Input.js';
 import Button from '../../components/Button/Button.js';
 
 export const MonthAddEditView = connect(monthAddEditViewSelector, monthAddEditViewActions)((props) => {
-
+    const custom = {date:"2016-05-15"}
     return <View>
         <TextLabel label={'Тут будет хэдер.'} />
 
@@ -30,6 +31,30 @@ export const MonthAddEditView = connect(monthAddEditViewSelector, monthAddEditVi
         <Button
             label={ 'Сохранить' }
             onPressButton={ props[SAVE_ITEM] }
+        />
+        <DatePicker
+            style={{width: 200}}
+            date={custom.date}
+            mode="date"
+            placeholder="select date"
+            format="YYYY-MM-DD"
+            minDate="2016-05-01"
+            maxDate="2016-06-01"
+            confirmBtnText="Confirm"
+            cancelBtnText="Cancel"
+            customStyles={{
+                dateIcon: {
+                    position: 'absolute',
+                    left: 0,
+                    top: 4,
+                    marginLeft: 0
+                },
+                dateInput: {
+                    marginLeft: 36
+                }
+                // ... You can check the source to find the other keys.
+            }}
+            onDateChange={(date) => console.log('MyDate:', date)}
         />
     </View>
 });
