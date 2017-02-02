8f915113817067201260918e67ce7ebc7fee7a97
ximet <dq.ximet@gmail.com>
2017-02-02 19:27:28 +0300

    add android full package

---
+++ android/app/build/generated/source/buildConfig/release/com/main/beta/update/appl/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.main.beta.update.appl;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.gohealthy";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
