bc25ea9618447e0fd3aa4fcc91e9a5a41564a6a1
ximet <dq.ximet@gmail.com>
2017-03-06 18:48:27 +0300

    update calendarList component

--- components/CalendarList/CalendarList.js
+++ components/CalendarList/CalendarList.js
@@ -1,36 +1,68 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableHighlight } from 'react-native';
 import Calendar from '../Calendar/Calendar.js';
 import Icon from '../Icon/Icon.js';
 
 import styles from './CalendarListStyles.js';
 
-const CalendarList = (props) => {
+class CalendarList extends React.Component {
+    constructor(props) {
+        super(props);
 
-    return (
-        <View style={ styles.CalendarListComponent }>
-            <View style={ styles.CalendarListHeader }>
-                <Text style={ styles.CalendarListTitle }>
-                    { props.titleCalendar }
-                </Text>
-            </View>
-            <View>
-                <View style={ styles.CalendarListHidePanel }>
-                    <Icon name={'keyboard-arrow-down'} size={45}/>
+        this.state = {
+            showCancel: false
+        }
+    }
+
+
+    toggleCancel () {
+        this.setState({
+            showCancel: !this.state.showCancel
+        });
+    }
+
+    renderCancel () {
+        if (this.state.showCancel) {
+            return (
+                <TouchableHighlight
+                    onPress={this.toggleCancel()}>
+                    <View>
+                        <Text style={styles.cancelButtonText}>Cancel</Text>
+                    </View>
+                </TouchableHighlight>
+            );
+        } else {
+            return null;
+        }
+    }
+
+    render () {
+        return (
+            <View style={ styles.CalendarListComponent }>
+                <View style={ styles.CalendarListHeader }>
+                    <Text style={ styles.CalendarListTitle }>
+                        { this.props.titleCalendar }
+                    </Text>
                 </View>
-                <Calendar
-                    events={ props.eventsCalendar }
-                    scrollEnabled
-                    showControls
-                    titleFormat={ props.titleFormat }
-                    dayHeadings = { props.dayHeadings }
-                    today={ props.today }
-                    onDateSelect={ props.onDateSelect }
-                />
+                <View>
+                    <View style={ styles.CalendarListHidePanel }>
+                        {this.renderCancel()}
+                        <Icon name={'keyboard-arrow-down'} size={45}/>
+                    </View>
+                    <Calendar
+                        events={ this.props.eventsCalendar }
+                        scrollEnabled
+                        showControls
+                        titleFormat={ this.props.titleFormat }
+                        dayHeadings={ this.props.dayHeadings }
+                        today={ this.props.today }
+                        onDateSelect={ this.props.onDateSelect }
+                    />
+                </View>
             </View>
-        </View>
-    );
-};
+        );
+    }
+}
 
 CalendarList.propTypes = {
     titleCalendar: React.PropTypes.string,
